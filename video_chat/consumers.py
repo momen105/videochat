9f0d2ba81e17fba8bbb089272b156af5822e6fe6
tohin <momentohin02@gmail.com>
2022-07-17 16:11:47 +0600

    update

--- video_chat/consumers.py
+++ video_chat/consumers.py
@@ -9,6 +9,7 @@ class VideoChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+        await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -17,24 +18,36 @@ class VideoChatConsumer(AsyncWebsocketConsumer):
         )
         print('disconnect')
 
-    async def receive(self, text_data=None, bytes_data=None):
-        # receive_dict = json.loads(text_data)
-        receive_dict = json.load(text_data)
+    async def receive(self, text_data):
+        receive_dict = json.loads(text_data)
         message = receive_dict['message']
+        action = receive_dict['action']
+
+        if (action == 'new-offer') or (action == 'new-answer'):
+            receiver_channel_name = receive_dict['message']['receiver_channel_name']
+            receive_dict['message']['receiver_channel_name'] = self.channel_name
+            await self.channel_layer.send(
+                receiver_channel_name,
+                {
+                    'type': 'send.sdp',
+                    'receive_dict': receive_dict
+                }
+            )
+            return
+
+        receive_dict['message']['receiver_channel_name'] = self.channel_name
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'send.message',
-                'message': message
+                'type': 'send.sdp',
+                'receive_dict': receive_dict
             }
         )
 
     # async def receive(self, text_data=None, bytes_data=None):
 
-    async def send_message(self, event):
-        message = event['message']
+    async def send_sdp(self, event):
+        receive_dict = event['receive_dict']
 
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.send(text_data=json.dumps(receive_dict))
